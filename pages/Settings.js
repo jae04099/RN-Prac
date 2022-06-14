829935a3d637ff497ee96e175644c1b17ea9962c
gyurilee <jae04099@gmail.com>
2022-06-14 17:02:24 +0900

    component 해결해야함

---
+++ pages/Settings.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet, Text } from "react-native";
+
+const Settings = () => {
+  return (
+      <Text style={{ fontSize: 24, color: "white" }}>Home</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+ navHead:{
+    backgroundColor: "#4B778D",
+    padding: 16,
+    margin: 10,
+    borderRadius: 8,
+ }
+});
+
+
+export default Settings;
